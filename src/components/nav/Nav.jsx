bf64110a585e608981e4f6c256a54f8eef82c003
ibukunolouwa oyeniyi <ibukun46@gmail.com>
2022-12-09 03:22:15 +0100

    updated the portfolio

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -12,6 +12,7 @@ const Nav = () => {
       <a href="#" onClick={() => setActiveNav('#')} className={activeNav === "#" ? 'active' : ''}><AiOutlineHome/></a>
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === "#about" ? 'active' : ''}> <AiOutlineUser/></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === "#experience" ? 'active' : ''}><BiBook/></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === "#portfolio" ? 'active' : ''}><RiServiceLine/></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === "#contact" ? 'active' : ''}><BiMessageSquareDetail/></a>
     </nav>
   )
--- src/components/portfolio/Porfolio.jsx
+++ src/components/portfolio/Porfolio.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./portfolio.css"
 import PORTFOLIO1 from "../../assets/portfolio1.jpg"
-import PORTFOLIO2 from "../../assets/portfolio2.png"
+import PORTFOLIO2 from "../../assets/portfolio2.jpg"
 import PORTFOLIO3 from "../../assets/portfolio3.jpg"
 
 const Portfolio = () => {
@@ -12,33 +12,33 @@ const Portfolio = () => {
       <div className="container portfolio__container">
         <article className="portfolio__item">
           <div className="portfolio__item-image">
-            <img src={PORTFOLIO1} alt="portfolio1" />
+            <img src={PORTFOLIO1} alt="portfolio1"  height="250px"/>
           </div>
-          <h3>PISURV</h3>
+          <h3>STREETRATES</h3>
           <div className='portfolio__item-cta'>
-            <a href="https://github.com/ibkay998/piSurv" target="_blank" rel='noreferrer' className='btn ' >Github</a>
-            <a href="https://frontend-pisurv.herokuapp.com/" target="_blank" rel='noreferrer' className='btn btn-primary'>Live Demo</a>
+            <a href="https://github.com/workshopapps/realxchangerate.web" target="_blank" rel='noreferrer' className='btn ' >Github</a>
+            <a href="https://streetrates.hng.tech/" target="_blank" rel='noreferrer' className='btn btn-primary'>Live Demo</a>
           </div>
             
         </article>
         <article className="portfolio__item">
           <div className="portfolio__item-image">
-            <img src={PORTFOLIO2} alt="portfolio2" />
+            <img src={PORTFOLIO2} alt="portfolio2" height="250px" />
           </div>
-            <h3>CHAT APP</h3>
+            <h3>MUSICA</h3>
             <div className='portfolio__item-cta'>
-              <a href="https://github.com/ibkay998/piSurv" target="_blank" rel='noreferrer' className='btn '>Github</a>
-              <a href="https://chatapp-420.herokuapp.com/login" target="_blank" rel='noreferrer' className='btn btn-primary'>Live Demo</a>
+              <a href="https://ibkay-musica.vercel.app" target="_blank" rel='noreferrer' className='btn '>Github</a>
+              <a href="https://github.com/ibkay998/Musica" target="_blank" rel='noreferrer' className='btn btn-primary'>Live Demo</a>
             </div>
         </article>
         <article className="portfolio__item">
           <div className="portfolio__item-image">
-            <img src={PORTFOLIO3} alt="portfolio1"/>
+            <img src={PORTFOLIO3} alt="portfolio1" height="250px"/>
           </div>
-            <h3>Social-Book</h3>
+            <h3>TUTERIA</h3>
             <div className='portfolio__item-cta'>
-              <a href="https://github.com/ibkay998/piSurv" target="_blank" rel='noreferrer' className='btn '>Github</a>
-              <a href="https://social-booknew.herokuapp.com/" target="_blank" rel='noreferrer' className='btn btn-primary'>Live Demo</a>
+              <a href="#" rel='noreferrer' className='btn '>Github</a>
+              <a href="https://tuteria.com/" target="_blank" rel='noreferrer' className='btn btn-primary'>Live Demo</a>
             </div>
         </article>
       </div>
